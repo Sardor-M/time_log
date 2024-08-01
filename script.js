5136cf0902ec820f4d023147940f54d6ef677f81
Sardor-M <sardor0968@g.seoultech.ac.kr>
2024-08-01 18:28:44 +0900

    feat: added initial setup

---
+++ script.js
@@ -0,0 +1,17 @@
+(function (d) {
+  "use strict";
+  let tableRowCellWidth = 90,
+    tableRowCellHeight = 52,
+    d = table.createElement("table"),
+    birhtdayInput = d.getElementById("birth-date"),
+    calendar = d.getElementById("calendar"),
+    submitBtn = d.getElementById("submit"),
+    weeksLivedSpan = d.getElementById("weeksRemaining"),
+    tableContainer = d.getElementById("table-container"),
+    resultContainer = d.getElementById("result"),
+    popUp = d.getElementById("pop-up"),
+    popUpTableContainer = d.getElementById("popup-table-container"),
+    popUpWeeksLeft = d.getElementById("popup-weeks-left"),
+    popUpYearsLeft = d.getElementById("popup-years-progress"),
+    closeBtn = d.getElementById("exit")[0];
+});
