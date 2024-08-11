22032efaebb0b862fac651a8b64d6235025d9941
Sardor-M <sardor0968@g.seoultech.ac.kr>
2024-08-11 17:21:12 +0900

    added popup card

--- script.js
+++ script.js
@@ -10,11 +10,11 @@
     weeksRemainingSpan = d.getElementById("weeks-remaining"),
     tableContainer = d.getElementById("table-container"),
     resultContainer = d.getElementById("result"),
-    popUp = d.getElementById("pop-up"),
+    popUp = d.getElementById("popup"),
     popUpTableContainer = d.getElementById("popup-table-container"),
-    popUpWeeksLeft = d.getElementById("popup-weeks-left"),
-    popUpYearsLeft = d.getElementById("popup-years-progress");
-  // closeBtn = d.getElementById("exit")[0];
+    popUpWeeksLeft = d.getElementById("popup-week-left"),
+    popUpYearsLeft = d.getElementById("popup-year-progress"),
+    exitButton = d.getElementsByClassName("exit")[0];
 
   calendar.addEventListener("click", function () {
     birhtdayInput.type = "date";
@@ -25,46 +25,55 @@
     birhtdayInput.type = "text";
   });
 
+  // exit button
+  exitButton.addEventListener("click", function () {
+    popUp.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    event.target === popUp ? (popUp.style.display = "none") : "";
+  });
+
   submitBtn.addEventListener("click", handleCalendarChange);
 
   // function to handle the calendar date change
   function handleCalendarChange() {
-    let birthday = new Date(birhtdayInput.value);
+    var birthday = new Date(birhtdayInput.value);
     if (isNaN(birthday)) {
       resultContainer.style.display = "none";
       document.getElementById("blog").style.display = "block";
     } else {
       calculateResult(birthday);
       resultContainer.style.display = "block";
-      document.getElementById("blog").style.display = "none";
+      //   document.getElementById("blog").style.display = "none";
     }
   }
 
   function calculateResult(birthday) {
-    let today = new Date();
-    let weeksLived = Math.floor(
+    var today = new Date();
+    var weeksLived = Math.floor(
       Math.abs(birthday - today) / (1000 * 60 * 60 * 24 * 7)
     );
 
-    let weeksRemaining = tableRowSize * tableDataSize - weeksLived;
+    var weeksRemaining = tableRowSize * tableDataSize - weeksLived;
 
     weeksLivedSpan.textContent = weeksLived;
     weeksRemainingSpan.textContent = weeksRemaining;
 
     tableContainer.innerHTML = "";
-    for (let i = 0; i < tableRowSize; i++) {
-      let tableRow = d.createElement("tr");
-      for (let b = 0; b < tableDataSize; b++) {
-        let tableData = d.createElement("td");
-        let weekNum = i * tableDataSize + b;
-        let weekYear = new Date(
+    for (var i = 0; i < tableRowSize; i++) {
+      var tableRow = d.createElement("tr");
+      for (var b = 0; b < tableDataSize; b++) {
+        var tableData = d.createElement("td");
+        var weekNum = i * tableDataSize + b;
+        var weekYear = new Date(
           birthday.getTime() + weekNum * 7 * 24 * 60 * 60 * 1000
         ).getFullYear();
         tableData.setAttribute(
           "data-week",
           "Week" + weekNum + "(" + weekYear + ")"
         );
-        tableData.setAttribute("data-year", weekYear);
+        tableData.setAttribute("date-year", weekYear);
 
         if (weekNum < weeksLived) {
           tableData.classList.add("past");
@@ -86,14 +95,62 @@
           this.classList.remove("hover");
           this.style.transform = "scale(1)";
         });
-        // tableData.addEventListener("click", function (event) {
-        //   showPopup(birthday, event.target.getAttribute("data-year"));
-        // });
+        tableData.addEventListener("click", function (event) {
+          displayPopUp(birthday, event.target.getAttribute("date-year"));
+        });
 
         tableRow.appendChild(tableData);
       }
       table.appendChild(tableRow);
     }
     tableContainer.appendChild(table);
+  }
+
+  function displayPopUp(birthday, selectedYear) {
+    var now = new Date();
+    var currentYear = now.getFullYear();
+    var nextBirthday = new Date(birthday);
+    nextBirthday.setFullYear(currentYear);
+
+    if (nextBirthday < now) {
+      nextBirthday.setFullYear(currentYear + 1);
+    }
+
+    var weeksUntilNextBirthday = Math.ceil(
+      (nextBirthday - now) / (1000 * 60 * 60 * 24 * 7)
+    );
+    var yearProgress = Math.round((1 - weeksUntilNextBirthday / 52) * 100);
+
+    popUpTableContainer.innerHTML = "";
+    var popupTable = d.createElement("table");
+    popupTable.classList.add("popup-table");
+
+    for (var i = 0; i < 4; i++) {
+      var popupTableRow = d.createElement("tr");
+      for (var b = 0; b < 13; b++) {
+        var popupTableData = d.createElement("td");
+        var weekIndex = i * 13 + b;
+        var weekDate = new Date(nextBirthday);
+        weekDate.setDate(weekDate.getTime() - (52 - weekIndex) * 7);
+
+        weekDate < now
+          ? popupTableData.classList.add("past")
+          : weekDate > now
+          ? popupTableData.classList.add("future")
+          : popupTableData.classList.add("current");
+
+        popupTableRow.appendChild(popupTableData);
+      }
+      popupTable.appendChild(popupTableRow);
+    }
+    popUpTableContainer.appendChild(popupTable);
+
+    popUpYearsLeft.textContent = "Year in Progress: " + yearProgress + "%";
+    popUpYearsLeft.style.fontFamily = "Space Mono, monospace";
+
+    popUpWeeksLeft.textContent = "Weeks left: " + weeksUntilNextBirthday;
+    popUpWeeksLeft.style.fontSize = "16px";
+
+    popUp.style.display = "block";
   }
 })(document);
